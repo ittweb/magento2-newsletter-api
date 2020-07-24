929f8749d5aa158133fd66e275af9e061a2b0325
Roberto Bianchi <rbianchi@ittweb.net>
2020-07-24 18:31:35 +0200

    Add GET and DELETE method

--- Api/SubscriptionInterface.php
+++ Api/SubscriptionInterface.php
@@ -4,9 +4,23 @@ namespace Ittweb\Newsletter\Api;
 interface SubscriptionInterface
 {
     /**
+     * GET for Newsletter api
+     * @param string $email
+     * @return void
+     */
+    public function isSubscribed($email);
+
+    /**
      * POST for Newsletter api
      * @param string $email
      * @return void
      */
     public function subscribeByEmail($email);
+
+    /**
+     * DELETE for Newsletter api
+     * @param string $email
+     * @return void
+     */
+    public function deleteByEmail($email);
 }
--- Model/Subscription.php
+++ Model/Subscription.php
@@ -1,14 +1,20 @@
 <?php
 namespace Ittweb\Newsletter\Model;
 use Ittweb\Newsletter\Api\SubscriptionInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
 
 class Subscription implements SubscriptionInterface
 {
     protected $subscriberFactory;
 
-    public function __construct(\Magento\Newsletter\Model\SubscriberFactory $subscriberFactory)
+    public function __construct(
+        \Magento\Newsletter\Model\SubscriberFactory $subscriberFactory,
+        \Magento\Customer\Model\CustomerFactory $customer,
+        \Magento\Store\Model\StoreManagerInterface $storemanager)
     {
-        $this->subscriberFactory= $subscriberFactory;
+        $this->subscriberFactory = $subscriberFactory;
+        $this->customer = $customer;
+        $this->storemanager = $storemanager;
     }
 
     /**
@@ -18,6 +24,34 @@ class Subscription implements SubscriptionInterface
      */
     public function subscribeByEmail($email)
     {
-        $this->subscriberFactory->create()->subscribe($email);
+        $websiteId = $this->storemanager->getStore()->getWebsiteId();
+        $customerId = $this->customer->create()->setWebsiteId($websiteId)->loadByEmail($email)->getId();
+        if ($customerId) {
+            $this->subscriberFactory->create()->subscribeCustomerById($customerId);
+        } else {
+            $this->subscriberFactory->create()->subscribe($email);
+        }
+    }
+
+    /**
+     * @param string $email
+     * @return void
+     */
+    public function isSubscribed($email)
+    {
+        if (!$this->subscriberFactory->create()->loadByEmail($email)->isSubscribed()) {
+            throw new NoSuchEntityException(__('Email not subscribed'));
+        }
+    }
+
+    /**
+     * @param string $email
+     * @return void
+     */
+    public function deleteByEmail($email)
+    {
+        $websiteId = $this->storemanager->getStore()->getWebsiteId();
+        $customerId = $this->customer->create()->setWebsiteId($websiteId)->loadByEmail($email)->getId();
+        $this->subscriberFactory->create()->unsubscribeCustomerById($customerId);
     }
 }
